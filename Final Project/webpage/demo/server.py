bd1a6415bb0304be89e8c1f7927bc2fb6781cbd1
Andrew <andrewhtsai@gmail.com>
2021-05-17 01:13:50 -0400

    Webpage

--- Final Project/webpage/demo/server.py
+++ Final Project/webpage/demo/server.py
@@ -1,3 +1,15 @@
+from flask import Flask, Response,render_template
+from flask_socketio import SocketIO, send, emit
+from subprocess import Popen, call
+
+import time
+import json
+import socket
+
+import signal
+import sys
+from queue import Queue
+
 import paho.mqtt.client as mqtt
 import uuid
 import threading
@@ -27,8 +39,8 @@ def on_message(cleint, userdata, msg):
 	# if (msg.topic == topic_read)
     #if(msg.topic==topic_read):
     print(f"topic: {msg.topic} msg: {msg.payload.decode('UTF-8')}")
-    stepCount+=100    
-    print(stepCount)
+    stepCount+=200    
+    #print(stepCount)
     update=1
 
 def thread_function():
@@ -48,19 +60,57 @@ def thread_function():
 	port=8883)
 
 	client.loop_forever()
+	
 
+def thread_function3():
+	socketio.run(app, host='0.0.0.0', port=5000)
+	
+
+hostname = socket.gethostname()
+hardware = 'plughw:3,0'
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+audio_stream = Popen("/usr/bin/cvlc alsa://"+hardware+" --sout='#transcode{vcodec=none,acodec=mp3,ab=256,channels=2,samplerate=44100,scodec=none}:http{mux=mp3,dst=:8080/}' --no-sout-all --sout-keep", shell=True)
+
+
+@socketio.on('connect')
+def test_connect():
+    print('connected')
+    emit('after connect',  {'data':'Lets dance'})
+
+@socketio.on('ping-gps')
+def handle_message(val):
+    emit('pong-gps', stepCount)
+    #print(mpu.acceleration) 
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', hostname=hostname)
+
+def signal_handler(sig, frame):
+    print('Closing Gracefully')
+    audio_stream.terminate()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
     x = threading.Thread(target=thread_function, args = ())
-    x.start()	
+    x.start()
+    x3 = threading.Thread(target=thread_function3, args = ())
+    x3.start()	
     client = mqtt.Client(str(uuid.uuid1()))
     client.tls_set()
     client.username_pw_set('idd', 'device@theFarm')
     client.connect(
-        'farlab.infosci.cornell.edu',
-        port=8883)
+    'farlab.infosci.cornell.edu',
+    port=8883)
     while True:
-        if(update==1):
+        if (update == 1):
             print(str(stepCount))
             client.publish(topic_write, str(stepCount/goal_value))
             update=0
+	
